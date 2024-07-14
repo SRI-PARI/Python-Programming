55602cb7f702ac97c01d10dbb9dd59862ffd466d
SRI-PARI <paritalasrilakshmi99@gmail.com>
2024-07-14 23:59:46 +0530

    Add files via upload

---
+++ Python-Basic/Simple_Programs/31. Ascending.py
@@ -0,0 +1,7 @@
+l = list(map(int, input().strip().split(",")))
+for i in range(0,len(l)):
+    for j in range(i,len(l)):
+        if l[i]>l[j]:
+            l[i],l[j]=l[j],l[i]
+for i in l:
+    print(i,end=" , ")
